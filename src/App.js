5e2870ae572cd795b8b3cea2e31859588bf07317
KH Nazmul Ahamed <khnazmulahamedbd@gmail.com>
2023-09-13 01:43:10 +0600

    ahamed Push

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ const App = () => {
             </div>
           ) : (
             <div className="w-0 dark:bg-secondary-dark-bg">
-              <Sidebar />
+              <Sidebar/>
             </div>
           )}
           <div
